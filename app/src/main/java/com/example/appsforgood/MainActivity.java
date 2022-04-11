0ff707d0aa380acb97997a3c14db87d5bed55374
Gracie Sheng <graciesheng1@gmail.com>
2022-04-11 08:59:46 -0400

    Added intent to buttons on MainActivity and ForecastActivity

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.appsforgood;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //displayForecast();
+        //displayClothing();
+    }
+
+    public void performForecast(View view) {
+        Intent intent = new Intent(this, ForecastActivity.class);
+        startActivity(intent);
+    }
+
+    public void performClothing(View view) {
+        Intent intent = new Intent(this, ClothingActivity.class);
+        startActivity(intent);
     }
 }
